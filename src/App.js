288eff680175cfb4eb28bc066a38b9592e3014ab
Jean-Christophe Chalté <jc.chalte@processis.fr>
2023-01-25 14:48:24 +0100

    Initial commit with docker support

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         This has been done by me, or another plz !
         </p>
         <a
           className="App-link"
